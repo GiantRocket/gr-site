d027771a29b29371da3aaf56ddf664b38d9aaa8b
Mariano Cibelli <mcibelli@despegar.com>
2015-04-09 14:46:51 -0300

    testing new repo

---
+++ src/main/java/com/giantrocket/team/data/model/User.java
@@ -0,0 +1,46 @@
+package com.giantrocket.team.data.model;
+
+import java.util.Date;
+import java.util.List;
+
+
+
+public class User {
+	
+	private String userName;
+	private String email;
+	private String password;
+	private Date creationDate;
+	private List<UserRole> roles;
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+	public List<UserRole> getRoles() {
+		return roles;
+	}
+	public void setRoles(List<UserRole> roles) {
+		this.roles = roles;
+	}
+}
