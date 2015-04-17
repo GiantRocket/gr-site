37a828da630a214a796b2a00eba1e47ad6019d75
Mariano Cibelli <mcibelli@despegar.com>
2015-04-17 12:16:51 -0300

    agregado ssl, se debe agregar el ssl.properties

--- src/main/java/com/giantrocket/site/App.java
+++ src/main/java/com/giantrocket/site/App.java
@@ -1,22 +1,32 @@
 package com.giantrocket.site;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
 
+import org.eclipse.jetty.http.ssl.SslContextFactory;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.server.ssl.SslSelectChannelConnector;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
 public class App {
-    private static String CONFIG_FILE_PATH = "conf/startup.conf";
-
+    private static final String SSL_PROPERTIES = "conf/ssl.properties";
+	private static String CONFIG_FILE_PATH = "conf/startup.conf";
+    
+    
+    
     public static void main(String[] args) throws Exception {
 
         Config root = ConfigFactory.parseFile(getFile());
@@ -24,7 +34,7 @@ public class App {
         ContextHandlerCollection contexts = getDispatcherServletsContext(server);
 
         Server s = new Server();
-        s.setConnectors(new Connector[] {getConnector(server)});
+        s.setConnectors(new Connector[] {getConnector(server), getSslConnector(server)});
         s.setHandler(contexts);
 
         s.start();
@@ -33,6 +43,33 @@ public class App {
     private static Connector getConnector(Config server) {
         Connector connector = new SelectChannelConnector();
         connector.setPort(server.getInt("port"));
+        if (server.hasPath("header_size")) {            
+            connector.setRequestHeaderSize(server.getInt("header_size"));
+        }
+
+        return connector;
+    }
+    
+    @SuppressWarnings("deprecation")
+	private static Connector getSslConnector(Config server) throws IOException {
+    	Resource resource = new ClassPathResource(SSL_PROPERTIES);
+    	Properties props = PropertiesLoaderUtils.loadProperties(resource);
+    	SslContextFactory sslContextFactory = new SslContextFactory();
+
+        sslContextFactory.setKeyStore(props.getProperty("com.giantrocket.jks.path"));
+        sslContextFactory.setKeyStorePassword(props.getProperty("com.giantrocket.jks.passphrase"));
+        sslContextFactory.setTrustStore(props.getProperty("com.giantrocket.jks.path"));
+        sslContextFactory.setKeyManagerPassword(props.getProperty("com.giantrocket.jks.passphrase"));
+
+        SslSelectChannelConnector connector = new SslSelectChannelConnector(sslContextFactory);
+
+        connector.setMaxIdleTime(30000);
+        String port = props.getProperty("com.giantrocket.ssl.port");
+        connector.setPort(Integer.parseInt(port));
+        connector.setAcceptors(4);
+
+        connector.setReuseAddress(false);
+        
         if (server.hasPath("header_size")) {            
             connector.setRequestHeaderSize(server.getInt("header_size"));
         }
